256e59fbfa80ac75cdaae1fe32176490a0108576
draclo <draclo@naver.com>
2025-11-08 17:10:18 +0900

    괄호회전하기

---
+++ src/programmers/괄호회전하기/Solution.java
@@ -0,0 +1,67 @@
+package programmers.괄호회전하기;
+
+import java.util.*;
+
+public class Solution {
+
+    public String rotate(String s) {
+        StringBuilder sb = new StringBuilder();
+        Queue<Character> queue = new LinkedList<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            queue.add(s.charAt(i));
+        }
+
+        Character temp = queue.poll();
+        queue.offer(temp);
+        while (!queue.isEmpty()) {
+            sb.append(queue.poll());
+        }
+        return sb.toString();
+    }
+
+    public boolean valid(String s) {
+        Map<Character, Character> charMap = new HashMap<>();
+        charMap.put(']', '[');
+        charMap.put('}', '{');
+        charMap.put(')', '(');
+
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (!charMap.containsKey(c)) {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                if (stack.peek() == charMap.get(c)) {
+                    stack.pop();
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
+
+    public int solution(String s) {
+        int result = 0;
+
+        if (valid(s)) result++;
+
+        String str = rotate(s);
+        while (!s.equals(str)) {
+            if (valid(str)) result++;
+            str = rotate(str);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        String str = "[](){}";
+        System.out.println(s.solution(str));
+    }
+}
